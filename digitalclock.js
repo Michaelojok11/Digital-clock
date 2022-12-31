f1f5daef5b2e8b917217d5a6bdb784a3c569fc08
Michaelojok11 <michaelocan11@gmail.com>
2022-12-31 21:52:38 +0300

    update git

---
+++ digitalclock.js
@@ -0,0 +1,10 @@
+const hourE1 = document.getElementById ("hour");
+const minuteE1 = document.getElementById("minutes");
+const secondE1 = document.getElementById("seconds");
+const ampmE1 = document.getElementById ("ampm");
+
+function updateClock (){
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+}
